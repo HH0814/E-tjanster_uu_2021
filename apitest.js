de56f3a2b02f0f569a7ebfb097a3473c7e2d51c7
KayabaAkihiko2 <84513574+KayabaAkihiko2@users.noreply.github.com>
2021-05-20 15:13:19 +0200

    PROJEKT

---
+++ apitest.js
@@ -0,0 +1,17 @@
+$.getJSON(
+    "https://www.carboninterface.com/api/v1/vehicle_makes", "Authorization: Bearer yJhTzwDiAFu4dno1KjeYiQ", 
+    function(data){
+        console.log(data);
+    }
+);
+
+//AjJdSutC_Rzz2S7C11Bhgyfl6CiBfImkYMzqDU5xNPf5dRZMFdph-7HhnSqIgSLX
+
+/*    $.each(data, function(index, value){
+    console.log(value);
+
+    var vehiclename = value.;
+    var vehicleinfo =;
+    var imageURL = value;
+}
+*/
