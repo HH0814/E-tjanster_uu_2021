7c6c815d025e4da88c6e0579dc37da63c6b6dfd1
KayabaAkihiko2 <84513574+KayabaAkihiko2@users.noreply.github.com>
2021-05-21 13:15:46 +0200

    Update apitest.js

--- apitest.js
+++ apitest.js
@@ -1,17 +1,20 @@
+$.ajaxSetup({
+    headers : {   
+        'Authorization': 'Bearer yJhTzwDiAFu4dno1KjeYiQ', 'Content-Type' : 'application/json'
+    }
+  });
 $.getJSON(
-    "https://www.carboninterface.com/api/v1/vehicle_makes", "Authorization: Bearer yJhTzwDiAFu4dno1KjeYiQ", 
-    function(data){
+    "https://www.carboninterface.com/api/v1/vehicle_makes",
+    function(data) {
         console.log(data);
+
+    var car = data[0].data
+    
+    var name = car.attributes.name;
+    $(".name").append(name);
+    var number_of_models = car.attributes.number_of_models;
+    $(".number_of_models").append(number_of_models);
+    var id = car.id;
+    $(".id").append(id);
     }
 );
-
-//AjJdSutC_Rzz2S7C11Bhgyfl6CiBfImkYMzqDU5xNPf5dRZMFdph-7HhnSqIgSLX
-
-/*    $.each(data, function(index, value){
-    console.log(value);
-
-    var vehiclename = value.;
-    var vehicleinfo =;
-    var imageURL = value;
-}
-*/
